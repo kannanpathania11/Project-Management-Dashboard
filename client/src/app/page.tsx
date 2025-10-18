9b02cef257d2328c4ec08ef29506a7b0c1b169f9
kannanpathania11 <kannanpathania@gmail.com>
2025-10-19 00:58:16 +0530

    setup tailwind config

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-center bg-blue-500 text-white text-3xl font-bold">
+      Tailwind is working! 🎉
+    </main>
+  );
+}
